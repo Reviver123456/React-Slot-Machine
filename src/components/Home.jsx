819ad8070e7024bb12a45e62a8b0960eaf1e2d1e
Reviver <108503367+Reviver123456@users.noreply.github.com>
2024-03-15 15:07:39 +0700

    อัพเดข

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,18 +2,31 @@ import { useState, useEffect } from 'react';
 import '../css/style.css';
 
 function Home() {
-    const [position, setPosition] = useState([0, 0, 0]);
+    const [position, setPosition] = useState([]);
     const iconHeight = 188;
     const multiplier = Math.floor(Math.random() * (4 - 1) + 1);
     const speed = iconHeight * multiplier;
-    const start = (Math.floor(Math.random() * 9) * iconHeight) * -1;
     const [winner, setWinner] = useState(false);
+    const [score, setScore] = useState(0);
+
+    const positions = [-940, -188, -0, -376, -1316, -1504, -564, -752, -1128];
+
+    const scores = [5, 10, 15, 20, 25, 30, 35, 40, 45]; 
+
+    useEffect(() => {
+        finishHandler();
+    }, []);
 
     useEffect(() => {
         if (position.length === 3) {
             const first = position[0];
             const results = position.every(match => match === first);
             setWinner(results);
+            if (results) {
+                const index = positions.findIndex(pos => pos === first);
+                const positionScore = scores[index];
+                setScore(prevScore => prevScore + positionScore);
+            }
         }
     }, [position]);
 
@@ -24,25 +37,24 @@ function Home() {
 
     const finishHandler = () => {
         const newPositions = Array.from({ length: 3 }, () => {
-            const multiplier = Math.floor(Math.random() * (4 - 1) + 1);
-            const speed = iconHeight * multiplier;
-            const start = (Math.floor(Math.random() * 9) * iconHeight) * -1;
-            return start;
+            const randomIndex = Math.floor(Math.random() * positions.length);
+            return positions[randomIndex];
         });
         setPosition(newPositions);
     };
 
     return (
         <div>
-            <h1 style={{ color: 'white' }}>
-                {winner && 'Winner!'}
+            <h1 style={{ color: winner ? 'green' : 'red' }}>
+                {winner ? 'Winner!' : 'Loss'}
             </h1>
+            <h2 style={{ color: 'white' }}>คะแนน: {score}</h2>
             <div className={`spinner-container`}>
-                <div style={{ backgroundPosition: '0px ' + position[0] + 'px' }} className={`icons`} />
-                <div style={{ backgroundPosition: '0px ' + position[1] + 'px' }} className={`icons`} />
-                <div style={{ backgroundPosition: '0px ' + position[2] + 'px' }} className={`icons`} />
+                {position.map((pos, index) => (
+                    <div key={index} style={{ backgroundPosition: '0px ' + pos + 'px' }} className={`icons`} />
+                ))}
             </div>
-            <button aria-label='Play again.' onClick={handleClick}>spin</button>
+            <button aria-label='Play again.' onClick={handleClick} className='bt-spin'>spin</button>
         </div>
     );
 }
